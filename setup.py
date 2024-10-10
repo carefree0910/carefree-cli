24bbb8dbf11ddb2851078c42dfa9ef2f10c040e7
carefree0910 <syameimaru.saki@gmail.com>
2024-10-10 12:34:20 +0800

    📦️Bumped to `0.1.2`

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-VERSION = "0.1.1"
+VERSION = "0.1.2"
 DESCRIPTION = "A `cli` that helps you manage your commands"
 with open("README.md", encoding="utf-8") as f:
     LONG_DESCRIPTION = f.read()
